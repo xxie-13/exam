4063dc70a1a172858214f4d2b1a68a3bd536c8be
xx <zarieeeeel@gmail.com>
2026-03-19 11:57:58 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const itemController = require("../controllers/itemController");
 
-//Route to get all users
-router.get("/users", userController.getAllUsers);
+//Route to get all items
+router.get("/item", itemController.getAllItem);
 
-//Route to search a user by id
-router.get("/users/:id", userController.getUserById);
-//router.get('/users/:,ip_address',userController.getUserByIp);
-router.post("/users", userController.createUser);
-router.put("/users", userController.updateUser);
-router.delete("/users", userController.deleteUser);
+//Route to search an item by id
+router.get("/item/:id", itemController.getItemById);
+router.post("/item", itemController.createItem);
+router.put("/item", itemController.updateItem);
+router.delete("/items", itemController.deleteItem);
 
 module.exports = router;
