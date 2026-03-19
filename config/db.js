3407f2d6a2a0a815cb299d11fc5d1981a6f2e4e6
xx <zarieeeeel@gmail.com>
2026-03-19 19:31:14 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-/*const mysql=require('mysql2');
+const mysql=require('mysql2');
 
 const connection=mysql.createConnection({
     host:process.env.DB_HOST,
@@ -7,16 +7,4 @@ const connection=mysql.createConnection({
     database:process.env.DB_NAME
 });
 
-module.exports=connection;*/
-
-const mysql = require('mysql2');
-
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+module.exports=connection;
